eaa4dba50fd84061e1559c80bb6a5038598b0210
Chai Wei Jian <cwj092@gmail.com>
2022-03-20 14:51:42 +0000

    add basic state for number-generator

--- src/lib/number-generator.js
+++ src/lib/number-generator.js
@@ -1,5 +1,32 @@
-function sum(a, b) {
-  return a + b;
+const NumberGeneratorState = {
+	NotStarted: 'NotStarted',
+	WaitingSignal: 'WaitingSignal',
+	RecordingNumber: 'RecordingNumber',
+	Ended: 'Ended',
 }
 
-export default sum;
+class NumberGenerator {
+	/**
+	 * @param {number} n
+	 */
+	constructor(n) {
+		this.state = NumberGeneratorState.NotStarted;
+		this.n = n;
+		/** @type {number[]} */
+		this.numbers = [];
+	}
+	onSignal() {
+		if (this.state == NumberGeneratorState.Ended) {
+			throw new Error('cannot call onSignal when generation has ended');
+		}
+		this.state = NumberGeneratorState.RecordingNumber;
+		this.numbers.push(0);
+		if (this.numbers.length < this.n) {
+			this.state = NumberGeneratorState.WaitingSignal;
+		} else {
+			this.state = NumberGeneratorState.Ended;
+		}
+	}
+}
+
+export { NumberGeneratorState, NumberGenerator };
--- src/lib/number-generator.test.js
+++ src/lib/number-generator.test.js
@@ -1,6 +1,22 @@
-import sum from './number-generator';
+import {NumberGenerator, NumberGeneratorState} from './number-generator';
 import {test,expect} from '@jest/globals';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('create number generator', () => {
+	const g = new NumberGenerator(0);
+	expect(g.state).toBe(NumberGeneratorState.NotStarted);
 });
+
+test('stop when all number generated', () => {
+	const g = new NumberGenerator(2);
+	g.onSignal();
+	expect(g.state).toBe(NumberGeneratorState.WaitingSignal);
+	g.onSignal();
+	expect(g.state).toBe(NumberGeneratorState.Ended);
+});
+
+test('error when call ended generator onSignal', () => {
+	const g = new NumberGenerator(1);
+	g.onSignal();
+	expect(() => g.onSignal()).toThrowError('cannot call onSignal when generation has ended');
+});
+
